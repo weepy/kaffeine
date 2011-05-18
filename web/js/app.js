058c3862106df73091635caf24610100ca63a0fe
weepy <jonahfox@gmail.com>
2011-05-18 10:46:59 +0100

    web page - added failing test to implicit_vars

--- web/js/app.js
+++ web/js/app.js
@@ -4,62 +4,42 @@ $().ready(function() {
     $(this).replaceWith($("<pre>" + x + "</pre>"))
   })
   
-  $(window).sausage({page: "h2, h3"});
+  $(window).sausage({page: ".section"});
   
 })
 
-// (function(){
-//     /*
-//         This first part is just throwaway code to simulate a lazy loaded page.
-//     */
-//     var pages = [],
-//         ran = 5,
-//         $pages = $('.page-set'),
-//         i = 0,
-//         index = 0,
-//         loading = false;
-//     function addPage() {
-//         var h_page = (Math.floor((1000-499)*Math.random()) + 500)
-//         $pages
-//             .append('<li class="page" style="height:' + h_page + 'px;">Page ' + (index + 1) + '</li>')
-//             ;
-//         index ++;
-//     };
-//     for (i = 0; i < ran; i++)
-//     {
-//         addPage(i);
-//     }
-//     $(window).scroll(function(){
-//         if (loading)
-//         {
-//             return;
-//         }
-//         if  ($(window).scrollTop() > $(document).height() - $(window).height() - 200)
-//         {
-//             loading = true;
-//             setTimeout(function(){
-//                 addPage();
-//                 $(window)
-//                     .sausage('draw')
-//                     ;
-//                 loading = false;
-//             }, 250);
-//         }
-//     });
-//     /*
-//         This is where the plugin is initialized.
-//     */
-// 
-//     function t() {  
-//         var time = new Date();  
-//         return time.getTime();  
-//     }
-//     var s = t();   
-//     for (var i = 0; i < 50; i++)
-//     {  
-//         $(window)
-//             .sausage('draw')
-//             ;
-//     }
-//     $('#out').append((t() - s) + 'ms');
-// }());
+$(function(){
+  
+  $("pre")
+    .each(function() {
+      var x = $(this).html()
+      x = x.replace(/\n\s*\n/g, "\n") 
+      x = x.replace(/\n\/\//g, "\n\n//")
+      x = x.replace(/\n\/\*/g, "\n\n/*")
+      x = x.replace(/\n*$/,"")
+      $(this).html(x)
+    })
+    .each(function() {
+      $(this).addClass("javascript")
+    })
+    .wrap("<div class=code></div>")
+  
+})
+
+
+$(function() {
+  Kaffeine = require("kaffeine")
+  
+  $(".code")
+    .each(function() {
+      var kaf = $(this).find("pre").text()
+      console.log(kaf)
+      var K = new Kaffeine()
+      var text = K.compile(kaf)
+      text = text.replace(/^\s+/,"")
+      $(this).append("<pre class='javascript rhs'>" + text + "</pre>")
+    })
+})
+
+
+
